ef6fb0d5afdfae1135816ccd45d380f6379fec13
guilherme-dev14 <gsmedeiros3@ucs.br>
2025-08-18 16:12:42 -0300

    Mudança para banco de dados na nuvem

--- Backend/src/main/java/com/taskifyApplication/TaskifyApplication.java
+++ Backend/src/main/java/com/taskifyApplication/TaskifyApplication.java
@@ -1,13 +1,18 @@
 package com.taskifyApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TaskifyApplication {
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
 
-	public static void main(String[] args) {
-		SpringApplication.run(TaskifyApplication.class, args);
-	}
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
 
+        SpringApplication.run(TaskifyApplication.class, args);
+    }
 }
